e6f47bb0e43ba77e2de74796afed169738d4da9d
Kishor Shachinandan <kshachinandan@anromsocial.com>
2022-05-22 21:17:19 +0300

    Deprecated: AddChoice

--- src/bots/discord/commands/tod.js
+++ src/bots/discord/commands/tod.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { MessageEmbed } = require('discord.js')
 
-const { getTimeOfDeath, getTimeOfRespawn } = require('../utils/timeUtil.js')
+const { getTimeOfRespawn, HOUR } = require('../utils/timeUtil.js')
 const { getDropInfo } = require('../utils/dropUtil.js')
 const { getColor } = require('../utils/colorUtil.js')
 const { setRaidBossesOption } = require('../utils/optionUtil')
-const { toUTC } = require('../utils/timeUtil')
 const config = require('../config.json')
 const raidBosses = require('../data/rb.json')
 
@@ -22,9 +21,9 @@ module.exports = {
       option.setName(config.timeOfDeathAllianceOptionTitle)
             .setDescription(config.timeOfDeathAllianceOptionDescription)
             .setRequired(true)
-            .addChoice('new order', 'New Order')
-            .addChoice('insane', 'Insane')
-            .addChoice('other', 'other'))
+            .addChoices({ name: 'new order', value: 'New Order' })
+            .addChoices({ name: 'insane', value: 'Insane' })
+            .addChoices({ name: 'other', value: 'other' }))
     .addIntegerOption(option =>
       option.setName(config.timeOfDeathMinutesOptionTitle)
             .setDescription(config.timeOfDeathMinutesOptionDescription)
@@ -33,36 +32,66 @@ module.exports = {
       option.setName(config.timeOfDeathDropOptionTitle)
             .setDescription(config.timeOfDeathDropOptionDescription)
             .setRequired(false)),
-  async execute(interaction) {
+  async execute (interaction) {
     try {
       await interaction.deferReply({ ephemeral: true })
       const name = interaction.options.getString(config.raidBossNameOptionTitle)
       const boss = raidBosses.find(b => b.name === name)
+      const { chance } = boss
       const alliance = interaction.options.getString(config.timeOfDeathAllianceOptionTitle)
       const minutes = interaction.options.getInteger(config.timeOfDeathMinutesOptionTitle) || 0
-      const isDrop = boss.chance === 100
-                     || interaction.options.getBoolean(config.timeOfDeathDropOptionTitle)
+      const isDrop = chance === 100 || interaction.options.getBoolean(config.timeOfDeathDropOptionTitle)
       const user = await interaction.member.fetch()
+      const respawn = await getTimeOfRespawn(minutes, boss)
 
       const result = `
-DEATH: ${ name } [${ alliance }] ${ toUTC(getTimeOfDeath(minutes)) } ${ getDropInfo(isDrop, boss.chance) }
-RESPAWN: ${ getTimeOfRespawn(minutes, boss) }`
+DEATH: ${name} [${alliance}] ${respawn.tod} ${getDropInfo(isDrop, chance)}
+RESPAWN: ${respawn.text}`
 
       const embedMessage = new MessageEmbed()
-        .setColor(getColor(alliance, isDrop, boss.chance))
+        .setColor(getColor(alliance, isDrop, chance))
         .setTitle(result)
         .setAuthor({
           name: user.displayName,
-          url: `https://discordapp.com/users/${ user.id }`,
-          iconURL: `https://cdn.discordapp.com/avatars/${ user.id }/${ interaction.user.avatar }.png`
+          url: `https://discordapp.com/users/${user.id}`,
+          iconURL: `https://cdn.discordapp.com/avatars/${user.id}/${interaction.user.avatar}.png`
         })
         .setTimestamp()
+      const bossChannel = await interaction.guild.channels.cache.find(ch => ch.name === boss.channel)
+      const bossChannelService = await interaction.guild.channels.fetch(bossChannel.id)
+      await bossChannelService.send({ embeds: [embedMessage] })
+
+      const eventsList = await interaction.guild.scheduledEvents.fetch()
+      const prevEvent = eventsList.find(event => event.name === name)
+
+      if (prevEvent?.isActive()) {
+        await prevEvent.setStatus('COMPLETED')
+      } else if (prevEvent?.isScheduled()) {
+        await interaction.guild.scheduledEvents.delete(prevEvent.id)
+      }
+
+      const event = {
+        name,
+        description: `
+Start gathering a Command Channel for ${name}.
+Respawn: ${respawn.text}`,
+        scheduledStartTime: respawn.start - HOUR,
+        scheduledEndTime: respawn.end,
+        privacyLevel: 'GUILD_ONLY',
+        entityType: 'EXTERNAL',
+        entityMetadata: { location: 'L2 Reborn x1' }
+        // image: imageURI 800x400 px
+      }
+
+      const channel = await interaction.guild.channels.cache.find(ch => ch.name === config.mainChannel)
+      const mainChannelService = await interaction.guild.channels.fetch(channel.id)
+      const scheduledEvent = await interaction.guild.scheduledEvents.create(event)
+      const url = await scheduledEvent.createInviteURL({ channel })
+      await mainChannelService.send(url)
 
       await interaction.editReply(result)
-      const chn = interaction.guild.channels.cache.find(ch => ch.name === boss.channel)
-      const channel = await interaction.guild.channels.fetch(chn.id)
-      await channel.send({ embeds: [ embedMessage ] })
-    } catch (error) {
+    }
+    catch (error) {
       console.error(error)
     }
   }
--- src/bots/discord/utils/optionUtil.js
+++ src/bots/discord/utils/optionUtil.js
@@ -1,12 +1,20 @@
+const { SlashCommandStringOption } = require('@discordjs/builders')
 const raidBosses = require('../data/rb.json')
 
+/**
+ * @param {SlashCommandStringOption} option
+ * @param {string} name
+ * @param {string} description
+ * @param {boolean} required
+ * @return SlashCommandStringOption
+ */
 exports.setRaidBossesOption = (option, name, description, required) => {
     const initialOption = option
-        .setName(name)
-        .setDescription(description)
-        .setRequired(required)
+      .setName(name)
+      .setDescription(description)
+      .setRequired(required)
 
-    raidBosses.forEach(boss => initialOption.addChoice(boss.id, boss.name))
+    raidBosses.forEach(boss => initialOption.addChoices({ name: boss.id, value: boss.name }))
 
     return initialOption
 }
